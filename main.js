e5d294d8ab49a50ba05c32f4c228cc5bb65cf832
Sheikh <kenneth.sheikh@hotmail.com>
2024-01-18 14:11:34 +0100

    onclick functions

--- main.js
+++ main.js
@@ -18,3 +18,26 @@ saySomethingFunction()
 // addEventListner
 
 const button = document.querySelector(".btn");
+console.log(button);
+
+// way 1
+
+const handleClickTwo = function () {
+    console.log("you clicked me omg!");
+}
+
+button.addEventListener("click", function () {
+    console.log("you clicked on me omg!");
+});
+
+button.addEventListener("click", handleClick);
+
+function handleClick () {
+    console.log("you clicked me omg!")
+}
+
+// way 2
+
+button.onclick = function () {
+    console.log("omg you clicked me using onclick :s");
+}
