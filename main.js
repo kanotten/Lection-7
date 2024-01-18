dcb64ba28ddab6051e189f504cb27ba18fa5e445
Sheikh <kenneth.sheikh@hotmail.com>
2024-01-18 14:19:02 +0100

    added keypress events

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-
 // function expressions
 //function assigned to a variable
-const someFunction = function() {
-    console.log("hello i am Hesh");
-}
+const someFunction = function () {
+  console.log("hello i am Hesh");
+};
 
 // normal function
-function saySomethingFunction () {
-    console.log("hello Truls");
+function saySomethingFunction() {
+  console.log("hello Truls");
 }
 
-saySomethingFunction()
-
+saySomethingFunction();
 
 // 2
 
@@ -23,21 +21,31 @@ console.log(button);
 // way 1
 
 const handleClickTwo = function () {
-    console.log("you clicked me omg!");
-}
+  console.log("you clicked me omg!");
+};
 
 button.addEventListener("click", function () {
-    console.log("you clicked on me omg!");
+  console.log("you clicked on me omg!");
 });
 
 button.addEventListener("click", handleClick);
 
-function handleClick () {
-    console.log("you clicked me omg!")
+function handleClick() {
+  console.log("you clicked me omg!");
 }
 
 // way 2
 
 button.onclick = function () {
-    console.log("omg you clicked me using onclick :s");
-}
+  console.log("omg you clicked me using onclick :s");
+};
+
+button.onclick = function (event) {
+  console.log(event.target.value);
+  console.log(this.value);
+};
+
+// Q3 keypress funciton
+// detect onkeydown // this event will trigger when you press on
+// onKeyUp // triggers when key is released
+//dont use onKeyDown, only keypress
