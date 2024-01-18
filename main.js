f827d1c276a167aea59656d85e5b42c5c17c731c
Sheikh <kenneth.sheikh@hotmail.com>
2024-01-18 14:00:59 +0100

    added css

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+
+// function expressions
+//function assigned to a variable
 const someFunction = function() {
     console.log("hello i am Hesh");
 }
 
+// normal function
 function saySomethingFunction () {
     console.log("hello Truls");
 }
+
+saySomethingFunction()
+
+
+// 2
+
+// addEventListner
+
+const button = document.querySelector(".btn");
